e8bdb633f95a080cbe117a3848099b14270646fa
waganse <yosuke.sato@datorama.com>
2019-02-08 06:52:45 +0900

    add mutation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { withAuthenticator } from 'aws-amplify-react';
 import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
+  state = {
+    message: '',
+    messages: []
+  };
+
   render() {
+    const { messages } = this.state;
+    console.log({ messages });
     return (
       <div>Test</div>
     );
