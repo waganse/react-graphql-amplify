179283c78d0fc6b23e3b0614e8d54cc3522033d1
Yosuke Sato <yosukesato@TKCWMMB29.local>
2019-02-07 16:15:10 +0900

    add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 
 class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>Test</div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
